014f2fb232e76bfd3821aa18fa0b384ccb63eea8
rolandkeszeg <78346746+keszegroland@users.noreply.github.com>
2024-01-23 12:54:02 +0100

    fetch location list data

--- pokemon-game/src/App.js
+++ pokemon-game/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import LocationList from './Components/Locations/LocationList';
 
 function App() {
   return (
     <div className="App">
-      
+      <LocationList></LocationList>
     </div>
   );
 }

c54216c2d64f2ce74e6e2513fefe02eb14399c6f
Adam Moór <moor.adam.is@gmail.com>
2024-01-24 13:34:07 +0100

    pokémon list completed

--- pokemon-game/src/App.js
+++ pokemon-game/src/App.js
@@ -1,9 +1,40 @@
 import './App.css';
 import LocationList from './Components/Locations/LocationList';
 import { useEffect, useState } from "react";
+import PokemonList from './Components/Locations/PokemonList';
+
+
 
 function App() {
+
+  const usersPokemon = [
+    "https://pokeapi.co/api/v2/pokemon/bulbasaur",
+    "https://pokeapi.co/api/v2/pokemon/charizard",
+    "https://pokeapi.co/api/v2/pokemon/poliwhirl"
+  ]
+
+
   const [locations, setLocations] = useState([]);
+  const [pokemonList, setPokemonList] = useState([])
+  const [pageState, setPageState] = useState("pokemonList");
+
+  useEffect(()=>{
+    async function getPokemons() {
+      const pokePromisses = usersPokemon.map(p => {
+        const promis = fetch(p)
+          .then((promis) => promis.json())
+        return promis
+      })
+      Promise.all(pokePromisses)
+        .then((nextPromis) => {
+          setPokemonList(nextPromis)
+          console.log(nextPromis);
+        })
+  
+    }
+    getPokemons();
+  })
+
 
   useEffect(() => {
     async function fetchData() {
@@ -20,10 +51,17 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <LocationList onClick={onClick} locations={locations}></LocationList>
+    <div className="App" >
+      
+      {
+        pageState === "locations" ? (
+          <LocationList onClick={onClick} locations={locations}></LocationList>
+        ) : pageState === "pokemonList" ? (<PokemonList pokemonList={pokemonList}></PokemonList>) : ('battle PLACEHOLDER')
+      }
     </div>
   );
 }
+
+//<button onClick={() => (getPokemons())}>Pokemon</button>
 
 export default App;
